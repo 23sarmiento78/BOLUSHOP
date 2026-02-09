1c2ea5b9c265b9f60e6b398c1f60eadd6d9a067c
lksarmiento67 <tu-email@asociado-a-lksarmiento67.com>
2026-02-08 22:04:32 -0300

    first commit

--- bolushop/app/(shop)/layout.tsx
+++ bolushop/app/(shop)/layout.tsx
@@ -7,12 +7,6 @@ export default function ShopLayout({
 }) {
     return (
         <>
-            <Script
-                async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5416044136120955"
-                crossOrigin="anonymous"
-                strategy="afterInteractive"
-            />
             {children}
         </>
     );
